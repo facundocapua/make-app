b050f54c25cd9ce6a200c81339514a7e95385824
Facundo Capua <facundocapua@gmail.com>
2023-07-01 11:28:14 -0300

    feat(privacy): Add privacy policy page

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+import PrivacyPolicy from '@/components/Privacy/PrivacyPolicy'
+const Page: NextPage = () => {
+  return (<PrivacyPolicy />)
+}
+
+export default Page
