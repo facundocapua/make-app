a8ef45301202624f7299455df26b69ef03a5b3be
Facundo Capua <facundocapua@gmail.com>
2025-10-19 11:04:26 -0300

    feat(event): update share functionality to include QR code image and improve message clarity

--- src/components/Events/event-modal/request-review-button.tsx
+++ src/components/Events/event-modal/request-review-button.tsx
@@ -9,17 +9,30 @@ export default function RequestReviewButton ({ event }: Props) {
   const { fullName } = event
 
   const handleShare = async () => {
+    const url = 'https://app.makeapp.ar/qr-reviews.png'
+    const res = await fetch(url)
+    const blob = await res.blob()
+
     const shareData = {
+      files: [
+        new File([blob],
+          'review.png',
+          {
+            type: blob.type,
+            lastModified: new Date().getTime()
+          }
+        )
+      ],
       title: '¿Cómo te fue?',
       text: `Hola ${fullName}!
 Espero que te haya gustado tu maquillaje!
 Me encantaría recibir tus comentarios. 
 
-Ingresá al link y compartí tu experiencia.
+Ingresá al link, o escaneá el QR, y compartí tu experiencia.
 ¡Gracias!
 Fer Gutierrez Makeup
 
-https://g.page/r/CVc4A9CcScYMEAI/review
+https://g.page/r/CVc4A9CcScYMEBM/review
 `
     }
     if (navigator.canShare && navigator.canShare(shareData)) {
